bd69d55f1cb1192f6708926aab9aed396181bbd3
piavw <piavonwachenfelt@hotmail.com>
2019-09-16 13:11:55 +0200

    Modifies what is shown on the page:   - adds a header,   - adds an instructions-message,    - links the message through props.

--- src/Announcement.js
+++ src/Announcement.js
@@ -2,8 +2,12 @@ import React from 'react';
 
 const Announcement = (props) => {
     return (
-        <p>Hello from the Announcement component</p>
-    )
+        <>
+        <h2>Hello from the Announcement component I've created!</h2>
+        <p>Whatever you type in the box below will be printed:</p>
+        <p>{props.message}</p>
+        </>
+    );
 }
 
 export default Announcement;
