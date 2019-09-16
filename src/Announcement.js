2e4ceff3849950101b90bbd3b6d81d186ce7486b
piavw <piavonwachenfelt@hotmail.com>
2019-09-16 13:04:58 +0200

    Adds new file for component, links new file with App.js.

---
+++ src/Announcement.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Announcement = (props) => {
+    return (
+        <p>Hello from the Announcement component</p>
+    )
+}
+
+export default Announcement;
--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Announcement from './Announcement';
 
-
-function App() {
-  return (
-    <div className="App">
-
-    </div>
-  );
+export class App extends Component {
+  state = {
+    message: 'Hello World!'
+  }
+  inputHandler = (event) => {
+    this.setState({
+      message: event.target.value
+    })
+  }
+  render() {
+    return (
+      <>
+        <Announcement 
+        message={this.state.message} 
+        />
+      <input 
+      onChange={this.inputHandler}
+      />
+      </>
+    )
+  }
 }
 
 export default App;
